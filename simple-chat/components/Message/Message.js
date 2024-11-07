aa12000448a66f6a966cc6f69b5b6ae5c0aaa5b9
marataibatullin <marat.aibatullin@gmail.com>
2024-11-07 16:28:56 +0700

    Обновлен Messages

--- simple-chat/components/Message/Message.js
+++ simple-chat/components/Message/Message.js
@@ -1,30 +1,33 @@
 export function Message({ text, images, timestamp, fromUser, read }) {
     const messageDiv = document.createElement('div');
-    
     messageDiv.className = fromUser ? 'message-sent' : 'message-received';
-    
+
     const messageStatusIcon = read ? 'done_all' : 'done';
 
-    let messageContent = `
-        <div class="message-text">${text}</div>
-    `;
+    let messageContent = '';
+
+    if (text) {
+        messageContent += `
+            <div class="message-text">${text}</div>
+        `;
+    }
 
     if (images && images.length > 0) {
+        const flagOnlyImage = (images.length == 1 && !text) ? 'only-image' : '' 
         images.forEach(imageSrc => {
             messageContent += `
-                <img src="${imageSrc}" class="message-image">
+                <img src="${imageSrc}" class="message-image ${flagOnlyImage}">
             `;
         });
     }
 
     messageContent += `
-        <div class="message-info">
+        <div class="message-info ${text ? '' : 'overlay'}">
             ${new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-            <span class="message-status material-symbols-outlined">${messageStatusIcon}</span>
+            ${fromUser ? `<span class="message-status material-symbols-outlined">${messageStatusIcon}</span>` : ''}
         </div>
     `;
 
     messageDiv.innerHTML = messageContent;
-    
     return messageDiv;
 }
